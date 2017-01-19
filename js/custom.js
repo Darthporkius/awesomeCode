8ff1934feb28074f2eb9e4a5f6a8d66b06cb7469
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2017-01-19 16:19:46 +0100

    PhotoGallery changes

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,47 @@ $("#firstQuestion").click(function() {
 $(".readmore").click(function() {
     $(".morecontent").toggle(900);
 });
+
+//The code for the ImageGallery
+var currentImage;
+
+$(".imageContainer").click(function(e) {
+    currentImage = e.target;
+    console.info (currentImage);
+
+    $(e.target)
+
+    .css("position","fixed")
+    .css("z-index","2")
+    .css("cursor","default")
+
+    .animate({
+        width: "100%",
+        height: "100%",
+        margin: 0,
+        top: 0,
+        left: 0,
+    }, 900, function() { 
+
+        $(".close").show(900);
+
+    })
+
+    .clearQueue();
+    console.log(currentImage.queue);
+});
+
+$(".close").click(function() {
+$(".close").hide();
+$(currentImage)
+
+        .css("position","relative")
+            .css("z-index","1")
+                .css("cursor","pointer")
+
+        .animate({
+            width: "250px",
+            height: "250px",
+            margin: "10px" }, 900, function(){
+        });
+});
